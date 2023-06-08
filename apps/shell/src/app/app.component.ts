5585ce79857ea747fd75aabbc520c8b69e7a3552
christianseguras <christian.segura@segurosbolivar.com>
2023-06-08 17:29:15 -0500

    Initial commit

---
+++ apps/shell/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ciencuadras-v14-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'shell';
+}
